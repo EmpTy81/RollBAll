ff2e3747a6db9f71f70121b4c340f633dd4f1a29
EmpTy81 <trosen1021@gmail.com>
2020-03-06 10:54:52 -0500

    Initial commit

---
+++ Assets/MYNE/Scripts/Control.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour {
+    private Rigidbody rb;
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Debug.Log(moveHorizontal);
+        float moreVertical = Input.GetAxis("Vertical");
+        Debug.Log(moreVertical);
+
+        Vector3 movement = new Vector3(moveHorizontal,0,moreVertical);
+        rb.AddForce(movement*5);
+
+    }
+}
